88cbdcc2a595a62648946fbfb5fe672dc24976a8
NikodemStasiewicz <nikodemstasiewicz@gmail.com>
2024-04-08 10:20:12 +0200

    lekka rozbudowa glownej strony

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,20 +7,32 @@ const HomePage = () => {
 
   return (
     <div className="relative" style={{
-        backgroundImage: `url('https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        minHeight: '100vh',
-      }}>
-      <div className="flex justify-between items-center p-4">
-        <div></div> 
-        <button
-          className="absolute top-4 right-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={handleSignInClick}
-        >
-          Sign in
-        </button>
+      position: 'relative',
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundImage: `url('https://user-images.githubusercontent.com/33485020/108069438-5ee79d80-7089-11eb-8264-08fdda7e0d11.jpg')`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}>
+      <div className="absolute inset-0 bg-black opacity-50"></div> {/* Nakładanie półprzezroczystego koloru */}
+      <button
+        className="absolute top-4 right-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleSignInClick}
+      >
+        Sign in
+      </button>
+      <div className="text-center text-white z-10 relative">
+        <h1 className='font-bold text-5xl'>Unlimited movies, TV shows, and more</h1>
+        <p className='py-5 text-2xl'>Watch anywhere. Cancel anytime</p>
+        <p className='text-2xl'>Ready to watch? Enter your email to create or restart your membership.</p>
+        <div className='relative  items-center inline-flex'>
+        <input type="text" id="enter_email" className=" text-white block opacity-50 rounded-none px-2.5 pb-2.5 pt-4 w-80 text-sm text-gray-900 bg-black dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+        <label htmlFor="enter_email" className=" absolute text-sm text-gray-400 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">Enter Email</label> <button className='bg-red-500 hover:bg-red-700 text-white font-bold px-2.5 pb-2.5 pt-4 rounded'>Get started</button>
       </div>
+      </div>
+
     </div>
   );
 };
